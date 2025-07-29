6aa148e2517ec49812b62bc42013549ddb6943eb
Matias Olivera <matias.olivera@naranjax.com>
2025-07-28 23:18:46 -0300

    feat(server): started listeners package

--- server/internal/core/listeners/http.go
+++ server/internal/core/listeners/http.go
@@ -1,1 +1,18 @@
 package listeners
+
+type HttpListener struct {
+	Id              string   `json:"id"`
+	Name            string   `json:"name"`
+	Port            int      `json:"port"`
+	Secure          bool     `json:"secure"`
+	Hosts           []string `json:"hosts"`
+	LiveSince       string   `json:"live_since"`
+	Status          string   `json:"status"`
+	Cert            Cert     `json:"cert,omitempty"` // Optional, only if Secure is true
+	ResponseHeaders []string `json:"response_headers,omitempty"`
+}
+
+type Cert struct {
+	CertFile string `json:"cert_file"`
+	Key      string `json:"key"`
+}
