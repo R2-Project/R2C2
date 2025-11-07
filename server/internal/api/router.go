a05bcfa7c6de910bd51bffc38e433474171124b5
Mati <matyv3@gmail.com>
2025-11-07 12:44:46 -0300

    feat(server): create listeners

--- server/internal/api/router.go
+++ server/internal/api/router.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mati-olivera/R2C2/internal/core/listeners"
 )
 
 func StartServer(port int) error {
@@ -14,6 +15,24 @@ func StartServer(port int) error {
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+
+	router.POST("/listeners/http", func(c *gin.Context) {
+
+		var data listeners.NewHttpListenerRequest
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		listener := listeners.NewHttpListener(data)
+
+		c.JSON(http.StatusCreated, gin.H{
+			"message": "Listener Created",
+			"data":    listener,
 		})
 	})
 
