a5325c554116d38df5682dd1df090ca3c08b7836
Mati <matyv3@gmail.com>
2026-03-03 16:34:19 -0300

    feat(server): setup config

--- server/cmd/cli/root.go
+++ server/cmd/cli/root.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/mati-olivera/R2C2/cmd/cli/server"
+	"github.com/mati-olivera/R2C2/internal/api"
+	"github.com/mati-olivera/R2C2/internal/config"
+	"github.com/mati-olivera/R2C2/internal/core/auth"
+	"github.com/mati-olivera/R2C2/internal/database"
 	"github.com/spf13/cobra"
 )
 
+var start bool
+
 func init() {
-	rootCmd.AddCommand(server.ServerCmd)
+	rootCmd.Flags().BoolVar(&start, "start", false, "Start the R2C2 server")
 	rootCmd.PersistentFlags().StringP("config", "c", "", "Path to the configuration file")
 }
 
@@ -18,8 +23,44 @@ var rootCmd = &cobra.Command{
 	Aliases: []string{"r2"},
 	Short:   "R2C2 command and control",
 	Long:    "R2C2 is a command and control server for red teaming post-explotation operations.\nComplete documentation is available at https://github.com/mati-olivera/r2c2/wiki.",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("IMPRIMI UN LOGO EN ASCII RE CHETO ACA")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if start {
+			configPathFlag, _ := cmd.Flags().GetString("config")
+			if configPathFlag == "" {
+				return fmt.Errorf("the --config flag with the configuration file path is required")
+			}
+			conf, err := config.LoadConfig(configPathFlag)
+			if err != nil {
+				return fmt.Errorf("failed to get configuration: %w", err)
+			}
+			db, err := config.InitDatabase(conf.DatabasePath)
+			if err != nil {
+				return fmt.Errorf("failed to initialize database: %w", err)
+			}
+
+			authRepo := database.InitOperatorsRepository(db.GetInstance())
+			authService := auth.NewAuthService(authRepo)
+			_, err = authService.LoadOperator(conf)
+			if err != nil {
+				return fmt.Errorf("failed to load default operator: %w", err)
+			}
+
+			// Start Server
+			err = api.StartServer(conf.Api.Port)
+			if err != nil {
+				return fmt.Errorf("failed to start server: %w", err)
+			}
+			return nil
+		}
+
+		fmt.Println(`
+ ____  ____   ___ ____
+|  _ \|___ \ / __|___ \
+| |_) | __) | |    __) |
+|  _ < / __/| |__ / __/
+|_| \_\_____|\___|_____|
+`)
+		return cmd.Help()
 	},
 }
 
